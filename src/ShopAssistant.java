605dc5ba1400e87c2676a9815393e99f977ad95d
Sylwester Leyk <sylwester95leyk@gmail.com>
2023-01-30 20:27:39 +0100

    zastosowanie tzw.camelCase w nazwach intrukcji oraz zamiana & koniunkcji na && koniunkcje ze skracaniem

--- src/ShopAssistant.java
+++ src/ShopAssistant.java
@@ -1,16 +1,16 @@
 public class ShopAssistant {
     public static void main(String[] args) {
-        double FirstPc = 12444;
-        double SecondPc = 2698;
-        double ThirdPc = 1198;
+        double firstPc = 12444;
+        double secondPc = 2698;
+        double thirdPc = 1198;
 
-        if (FirstPc <= SecondPc & FirstPc <= ThirdPc) {
+        if (firstPc <= secondPc && firstPc <= thirdPc) {
             System.out.println("Najtańszy jest pierwszy komputer");
         }
-        if (SecondPc <= FirstPc & SecondPc <= ThirdPc) {
+        if (secondPc <= firstPc && secondPc <= thirdPc) {
             System.out.println("Najtańszy jest drugi komputer");
         }
-        if (ThirdPc <= SecondPc & ThirdPc <= FirstPc) {
+        if (thirdPc <= secondPc && thirdPc <= firstPc) {
             System.out.println("Najtańszy jest trzeci komputer");
         }
     }
