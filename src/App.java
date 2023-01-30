5ab25b5d88bb1ffba17df715d2d948e097b71e91
Sylwester Leyk <sylwester95leyk@gmail.com>
2023-01-30 19:40:34 +0100

    #6 Dodanie pliku App zawierającego wykonane zadanie

---
+++ src/App.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) {
+        int number1 = 1;
+        int number2 = 2;
+        int number3 = 22;
+        int number4 = 783;
+        int number5 = 1;
+
+        if (number1 * 2 + 4 < 8) {
+            System.out.println("Tak");
+        } else {
+            System.out.println("Nie");
+        }
+        if (number2 * 2 + 4 < 8) {
+            System.out.println("Tak");
+        } else {
+            System.out.println("Nie");
+        }
+        if (number3 * 2 + 4 < 8) {
+            System.out.println("Tak");
+        } else {
+            System.out.println("Nie");
+        }
+        if (number4 * 2 + 4 < 8) {
+            System.out.println("Tak");
+        } else {
+            System.out.println("Nie");
+        }
+        if (number5 * 2 + 4 < 8) {
+            System.out.println("Tak");
+        } else {
+            System.out.println("Nie");
+        }
+    }
+}
